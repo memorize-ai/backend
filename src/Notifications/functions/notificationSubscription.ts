8936d47a5f67738cfe9a271477cf365bbd98de8c
kenmueller <kenmueller0@gmail.com>
2021-01-23 21:30:39 -0800

    notifications allocate more time and 5 minute interval

--- src/Notifications/functions/notificationSubscription.ts
+++ src/Notifications/functions/notificationSubscription.ts
@@ -5,20 +5,26 @@ import getNotification from '..'
 import Notification, { sendNotifications } from '../Notification'
 import { cauterize } from '../../utils'
 
+const TIME_STEP = 5
+const SCHEDULE = `*/${TIME_STEP} * * * *`
+
 const firestore = firebase.firestore()
 
-export default functions.pubsub.schedule('every 1 minutes').onRun(
-	cauterize(async () => {
-		const { docs: users } = await firestore.collection('users').get()
-		const notifications: Notification[] = []
+const collect = async () => {
+	const { docs: users } = await firestore.collection('users').get()
+	const notifications: Notification[] = []
 
-		await Promise.all(
-			users.map(async user => {
-				const notification = await getNotification(user)
-				if (notification) notifications.push(...notification)
-			})
-		)
+	await Promise.all(
+		users.map(async user => {
+			const notification = await getNotification(user)
+			if (notification) notifications.push(...notification)
+		})
+	)
 
-		await sendNotifications(notifications)
-	})
-)
+	await sendNotifications(notifications)
+}
+
+export default functions
+	.runWith({ timeoutSeconds: 540, memory: '2GB' })
+	.pubsub.schedule(SCHEDULE)
+	.onRun(cauterize(collect))
